2c56e95c4c9f9200791e7e4ac949631ee6983f42
rosewhitt <rosewhitt@icloud.com>
2018-06-05 19:48:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(int)
   if int %5 == 0 && int %3 == 0
-    puts "Fizzbuzz"
+    return "Fizzbuzz"
   elsif int %5 == 0
-    puts "Buzz"
+    return "Buzz"
   elsif int %3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif int %5 != 0 && int %3 != 0
-    puts "nil"
+    return "nil"
   end
 end
 
